03067a962835aadc9e360194303c1b427727c342
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-16 08:52:03 +0300

    Revert "This commit created the Quizinterface class which designs the window outlook of the application"
    This reverts commit cdbd35f50816a3e2d7312b5f220a3ca9fb91f056.

--- quiz_ui.py
+++
@@ -1,9 +0,0 @@
-class QuizInterface:
-
-    def __init__(self, quiz_brain: QuizBrain) -> None:
-        self.quiz = quiz_brain
-        self.window = Tk()
-        self.window.title("Panther Banter")
-        self.window.geometry("850x530")
-        # self.window.configure(bg='blue')
-        self.window['background']='#FAD586'
